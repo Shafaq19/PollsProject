e67cdeb894652775722f7864b324e15b697c37f9
shafaq19 <arshad.shafaq15@gmail.com>
2020-08-24 18:15:00 +0500

    customize admin site

--- mysite/polls/admin.py
+++ mysite/polls/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Question,Choice
+
+
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 3
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None,               {'fields': ['question_text']}),
+        ('Date information', {'fields': ['pub_date'], 'classes': ['collapse']}),
+
+    ]
+    list_display = ['question_text','pub_date','was_published_recently']
+    search_fields = ['question_text']
+    inlines = [ChoiceInline]
+
+admin.site.register(Question, QuestionAdmin)
--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -9,15 +9,22 @@ class Question(models.Model):
     pub_date = models.DateTimeField('date published')
 
     def __str__(self):
-        return "Question: %s"%(self.question_text)
+        return "Question: %s" % (self.question_text)
 
     def was_published_recently(self):
         now = timezone.now()
         return now - datetime.timedelta(days=1) <= self.pub_date <= now
 
+    was_published_recently.admin_order_field = 'pub_date'
+    was_published_recently.boolean = True
+    was_published_recently.short_description = 'Published recently?'
+
+
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=200)
+    pub_date = models.DateTimeField('date published')
     votes = models.IntegerField(default=0)
+
     def __str__(self):
-        return "Choice : %s\nVotes: %d"%(self.choice_text,(self.votes))
+        return "Choice : %s\nVotes: %d" % (self.choice_text, (self.votes))
